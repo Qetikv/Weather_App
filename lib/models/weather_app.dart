c7b502890f85e6cf0f947cd997f479ee9d24ef2a
unknown <qqetikv@gmail.com>
2024-02-26 00:48:16 +0400

    add weather conditions

--- lib/models/weather_app.dart
+++ lib/models/weather_app.dart
@@ -1,14 +1,17 @@
-// lib/models/weather_model.dart
 class Weather {
   final String cityName;
   final double temperature;
+  final String weatherCondition;
 
-  Weather({required this.cityName, required this.temperature});
+  Weather({required this.cityName, required this.temperature, required this.weatherCondition});
 
   factory Weather.fromJson(Map<String, dynamic> json) {
-    return Weather(
-      cityName: json['name'],
-      temperature: (json['main']['temp'] - 273.15),
-    );
-  }
+    final List<dynamic> weatherData = json['weather'];
+    final String condition = weatherData.isNotEmpty ? weatherData[0]['main'] : 'Unknown';
+      return Weather(
+        cityName: json['name'],
+        temperature: (json['main']['temp'] - 273.15),
+        weatherCondition: condition,
+      );
+    }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -9,19 +9,32 @@ class WeatherApp extends StatefulWidget {
 
 class _WeatherAppState extends State<WeatherApp> {
   final TextEditingController _cityController = TextEditingController();
-  String _weatherInfo = '';
+  Weather _weatherInfo = Weather(cityName: '', temperature: 0, weatherCondition: '');
 
   Future<void> _fetchWeather(String cityName) async {
     try {
       final Map<String, dynamic> data = await WeatherService.fetchWeather(cityName);
       final Weather weather = Weather.fromJson(data);
       setState(() {
-        _weatherInfo = 'City: ${weather.cityName}, Temperature: ${weather.temperature.toStringAsFixed(0)}°C';
+        _weatherInfo = weather;
       });
     } catch (e) {
       setState(() {
-        _weatherInfo = 'Failed to load weather data';
+        _weatherInfo = Weather(cityName: 'Unknown', temperature: 0, weatherCondition: 'Unknown');
       });
+    }
+  }
+
+    // Function to get the weather image URL based on the weather condition
+  String _getWeatherImageURL(String weatherCondition) {
+    if (weatherCondition == 'Clear') {
+      return 'assets/clear.png';
+    } else if (weatherCondition == 'Clouds') {
+      return 'assets/cloudy.png';
+    } else if (weatherCondition == 'Rain') {
+      return 'assets/rainy.png';
+    } else {
+      return 'assets/default.png';
     }
   }
 
@@ -29,30 +42,80 @@ class _WeatherAppState extends State<WeatherApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Weather App'),
+        title: const Text(
+          'Check Weather in Your City',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blueAccent,
+        elevation: 0,
+        centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+       body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blueAccent, Colors.white],
+          ),
+        ),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextField(
-              controller: _cityController,
-              decoration: InputDecoration(
-                labelText: 'Enter a city name',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    _fetchWeather(_cityController.text);
-                  },
+            Expanded(
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _cityController,
+                    decoration: InputDecoration(
+                      labelText: 'Enter a city name',
+                      labelStyle: const TextStyle(color: Colors.white),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.search),
+                        onPressed: () {
+                          _fetchWeather(_cityController.text);
+                        },
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
-            const SizedBox(height: 16.0),
-            Text(
-              _weatherInfo,
-              style: const TextStyle(fontSize: 18.0),
-            ),
+            _weatherInfo.cityName.isNotEmpty
+                ? Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'City: ${_weatherInfo.cityName}',
+                            style: const TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                          Text(
+                            'Temperature: ${_weatherInfo.temperature.toStringAsFixed(0)}°C',
+                            style: const TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                          Text(
+                            'Weather Condition: ${_weatherInfo.weatherCondition}',
+                            style: const TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                          const SizedBox(height: 16.0),
+                          Image.asset(
+                            _getWeatherImageURL(_weatherInfo.weatherCondition),
+                            width: 100,
+                            height: 100,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                : Container(),
           ],
         ),
       ),
